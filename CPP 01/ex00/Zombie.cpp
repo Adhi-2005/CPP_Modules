eed439bc8ab7679ab5660ba339fc22fd85baf6cf
adhi-2005 <adhilshafeer1@gmail.com>
2025-01-19 12:17:00 +0400

    CPP 01 started

---
+++ CPP 01/ex00/Zombie.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) :name(name)
+{
+	std::cout<< name << " is created...\n";
+}
+
+Zombie::~Zombie()
+{
+	std::cout<< "Zombie" << name << " Destroyed...\n";
+}
+
+void Zombie::announce() const{
+	std::cout<< name << ": BraiiiiiiinnnzzzZ...\n";
+}
