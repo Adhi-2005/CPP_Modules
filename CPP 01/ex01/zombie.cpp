0554d8c8e201b4aa98b0708ad8992663d7dd5c44
Adhi-2005 <adhilshafeer1@gmail.com>
2025-01-20 09:35:36 +0400

    CPP01 ex 01 started

--- CPP 01/ex01/zombie.cpp
+++ CPP 01/ex01/zombie.cpp
@@ -1,26 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adshafee <adshafee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 14:32:15 by adshafee          #+#    #+#             */
-/*   Updated: 2025/01/19 14:32:16 by adshafee         ###   ########.fr       */
+/*   Created: 2025/01/19 14:32:07 by adshafee          #+#    #+#             */
+/*   Updated: 2025/01/19 14:43:17 by adshafee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#include "zombie.hpp"
 
-int	main()
-{
-	std::string main_str= "HI THIS IS BRAIN";
-	std::string* stringPTR = &main_str;
-	std::string& stringREF = main_str;
-
-	std::cout<< "Output from the Main string: " << main_str << "\n";
-	std::cout<< "Memory Adrress pointer to by the stringPTR: " << stringPTR << "\n";
-	std::cout<< "Memory address held by stringREF: " << stringREF << "\n";
-	return (0);
-}
+Zombie::Zomboie() : name(""){}
--- CPP 01/ex01/zombie.hpp
+++ CPP 01/ex01/zombie.hpp
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adshafee <adshafee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 14:32:15 by adshafee          #+#    #+#             */
-/*   Updated: 2025/01/19 14:32:16 by adshafee         ###   ########.fr       */
+/*   Created: 2025/01/19 14:32:35 by adshafee          #+#    #+#             */
+/*   Updated: 2025/01/19 14:39:16 by adshafee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 #include <iostream>
 #include <string>
 
-int	main()
-{
-	std::string main_str= "HI THIS IS BRAIN";
-	std::string* stringPTR = &main_str;
-	std::string& stringREF = main_str;
+class Zombie{
+	private :
+		std::string name;
 
-	std::cout<< "Output from the Main string: " << main_str << "\n";
-	std::cout<< "Memory Adrress pointer to by the stringPTR: " << stringPTR << "\n";
-	std::cout<< "Memory address held by stringREF: " << stringREF << "\n";
-	return (0);
+	public :
+		Zombie* zombieHorde( int N, std::string name );
+		~Zombie();
+		
+		void announce() const;
 }
+
+#endif
