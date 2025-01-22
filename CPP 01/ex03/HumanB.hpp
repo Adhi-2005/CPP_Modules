8890350e5caa3439b62e31e0b6ea6d2ccf8e7bd8
Adhi-2005 <adhilshafeer1@gmail.com>
2025-01-22 19:31:35 +0400

    changes done

--- CPP 01/ex03/HumanB.hpp
+++ CPP 01/ex03/HumanB.hpp
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adhil <adhil@student.42.fr>                +#+  +:+       +#+        */
+/*   By: adshafee <adshafee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:01:19 by adhil             #+#    #+#             */
-/*   Updated: 2025/01/20 15:00:53 by adhil            ###   ########.fr       */
+/*   Updated: 2025/01/22 13:03:59 by adshafee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HUMANB_HPP
-# define HUMAN_HPP
+# define HUMANB_HPP
 
 #include "Weapon.hpp"
 
