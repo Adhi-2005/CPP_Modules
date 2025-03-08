cc57bdd5e2b64da26f56c05f5f3d30ed8da0a0c4
Adhi-2005 <adhilshafeer1@gmail.com>
2025-03-08 13:07:31 +0400

    made some spaces replaced with the tabs

--- CPP 01/ex03/main.cpp
+++ CPP 01/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adshafee <adshafee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adhi-2005 <adhi-2005@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 10:05:27 by adshafee          #+#    #+#             */
-/*   Updated: 2025/03/08 11:20:38 by adshafee         ###   ########.fr       */
+/*   Updated: 2025/03/08 13:06:46 by adhi-2005        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@
 int main() {
 	{
 		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);  // Bob is armed with a club
-		bob.attack();              // Outputs: Bob attacks with their crude spiked club
+		HumanA bob("Bob", club);	// Bob is armed with a club
+		bob.attack();			// Outputs: Bob attacks with their crude spiked club
 
 		club.setType("some other type of club");
-		bob.attack();              // Outputs: Bob attacks with their some other type of club
+		bob.attack();			// Outputs: Bob attacks with their some other type of club
 	}
 
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
 		jim.attack();
-		jim.setWeapon(club);  // Set weapon for Jim later
-		jim.attack();         // Outputs: Jim attacks with their crude spiked club
+		jim.setWeapon(club);	// Set weapon for Jim later
+		jim.attack();		// Outputs: Jim attacks with their crude spiked club
 
 		club.setType("some other type of club");
-		jim.attack();         // Outputs: Jim attacks with their some other type of club
+		jim.attack();		// Outputs: Jim attacks with their some other type of club
 	}
 
 	return 0;
