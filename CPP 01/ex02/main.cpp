160076caf33fb563e586ccdcc3b6cc77cecda1ab
Adhi-2005 <adhilshafeer1@gmail.com>
2025-03-08 10:03:21 +0400

    new changes added

--- CPP 01/ex02/main.cpp
+++ CPP 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adshafee <adshafee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 14:32:15 by adshafee          #+#    #+#             */
-/*   Updated: 2025/01/19 14:32:16 by adshafee         ###   ########.fr       */
+/*   Updated: 2025/03/08 10:02:43 by adshafee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,16 @@ int	main()
 	std::string* stringPTR = &main_str;
 	std::string& stringREF = main_str;
 
-	std::cout<< "Output from the Main string: " << main_str << "\n";
-	std::cout<< "Memory Adrress pointer to by the stringPTR: " << stringPTR << "\n";
-	std::cout<< "Memory address held by stringREF: " << stringREF << "\n";
+	// Printing out the Memory addresses
+	std::cout<< "Memory Address of the Main string: " << &main_str << "\n";
+	std::cout<< "Memory Address pointer to by the stringPTR: " << stringPTR << "\n";
+	std::cout<< "Memory Address held by stringREF: " << &stringREF << "\n";
+
+	std::cout<< "\n";
+
+	// Printing out the value stored
+	std::cout << "Value of the string variable: " << main_str << "\n";
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << "\n";
+	std::cout << "Value pointed to by stringREF: " << stringREF << "\n";
 	return (0);
 }
