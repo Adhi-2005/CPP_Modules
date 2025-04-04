97e31437bf7d04abba2f45c607a7583924987736
Adhi-2005 <adhilshafeer1@gmail.com>
2025-04-04 19:42:11 +0400

    CPP05 ex00 started

---
+++ CPP 05/ex00/Bureaucrat.cpp
@@ -0,0 +1,55 @@
+#include "Bureaucrat.hpp"
+
+const char*Bureaucrat::GradeTooHighExeption::what() const throw() {
+	return "Grade too high!";
+}
+
+const char*Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Grade too low!";
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade) {
+	if(grade < 1)
+		throw GradeTooHighExeption();
+	if(grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade){
+	std::cout << "Copy Constructor called\n";
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other){
+	if (this != &other)
+		this->grade = other.grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout<< "Destructor Called\n";
+}
+
+std::string Bureaucrat::getName()const{
+	return name;
+}
+
+int Bureaucrat::getGrade() const{
+	return grade;
+}
+
+void Bureaucrat::incrementGrade(){
+	if (grade - 1 < 1)
+		throw GradeTooHighExeption();
+	grade--;
+}
+
+void Bureaucrat::decrementGrade(){
+	if (grade + 1 > 150)
+		throw GradeTooLowException();
+	grade++;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() <<  ".";
+	return os;
+}
