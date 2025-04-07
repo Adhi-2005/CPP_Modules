37c64ec9cbf31d8dc59364556a278ab681079948
Adhi-2005 <adhilshafeer1@gmail.com>
2025-04-07 16:39:43 +0400

    just removed the commented part

--- CPP 05/ex01/Form.hpp
+++ CPP 05/ex01/Form.hpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <stdexcept>
 
-class Bureaucrat; // Forward declaration
+class Bureaucrat;
 
 class Form {
 private:
@@ -15,8 +15,7 @@ private:
 	const int gradeToExecute;
 
 public:
-	// Exception classes
-	class GradeTooHighException : public std::exception {
+		class GradeTooHighException : public std::exception {
 		public:
 			const char* what() const throw();
 	};
@@ -26,22 +25,18 @@ public:
 			const char* what() const throw();
 	};
 
-	// Constructors
 	Form(const std::string& name, int gradeToSign, int gradeToExecute);
 	Form(const Form& other);
 	Form& operator=(const Form& other);
 	~Form();
 
-	// Getters
 	std::string getName() const;
 	bool getIsSigned() const;
 	int getGradeToSign() const;
 	int getGradeToExecute() const;
 
-	// Member functions
 	void beSigned(const Bureaucrat& bureaucrat);
 
-	// Overload <<
 	friend std::ostream& operator<<(std::ostream& os, const Form& form);
 };
 
